4c75a46ef4cd9e3373de388802437e395359ea78
deepak prajapati <deepakprajapati@deepaks-MacBook-Air.local>
2024-12-09 02:19:09 +0530

    added atm

--- src/main/java/com/lld/atm/CashDispenser.java
+++ src/main/java/com/lld/atm/CashDispenser.java
@@ -1,7 +1,22 @@
 package com.lld.atm;
 
+import javax.naming.InsufficientResourcesException;
+
 public class CashDispenser {
     private int cashAvailable;
+
+    public CashDispenser(int initialCash) {
+        this.cashAvailable = initialCash;
+    }
+
+
+    public synchronized void dispenseCash(int amount) throws InsufficientResourcesException {
+        if(amount>cashAvailable){
+            throw new InsufficientResourcesException("Cash unavailable");
+        }
+        cashAvailable-=amount;
+        System.out.println("Cash dispensed: "+ amount);
+    }
 
 
 }
